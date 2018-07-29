bd02fcd3b30dceb137c42e60be83f6b0e7f2aa64
Steven Li <steven.li@walmartlabs.com>
2018-07-28 17:27:56 -0700

    web3 balances route complete

--- routes/balances.js
+++ routes/balances.js
@@ -20,7 +20,34 @@ router.get("/", function(req, res, next) {
 
 /* GET users listing. */
 router.post("/", function(req, res, next) {
-  res.send("respond with a resource");
+  // User address
+  var address = req.address || "0xda0aed568d9a2dbdcbafc1576fedc633d28eee9a";
+
+  // Token contract address]
+  var contractAddress =
+    req.contract || "0xa74476443119A942dE498590Fe1f2454d7D4aC0d";
+
+  // For for balanceOf: remove the 0x
+  var tknAddress = addr.substring(2);
+
+  // Format for balanceOf: add in the contract
+  var contractData = "0x70a08231000000000000000000000000" + tknAddress;
+
+  // Utilize web3 to call balance
+  web3.eth.call(
+    {
+      to: contractAddr, 
+      data: contractData 
+    },
+    function(err, result) {
+      if (result) {
+        var tokens = web3.utils.toBN(result).toString();
+        res.json({ balance: web3.utils.fromWei(tokens, "ether") });
+      } else {
+        res.json({ error: err });
+      }
+    }
+  );
 });
 
 module.exports = router;
